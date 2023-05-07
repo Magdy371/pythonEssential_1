9a32fa496b0e08cf483381b18684470de504c20e
Magdy371 <magdyelshrief371@gmail.com>
2023-05-07 15:14:24 +0300

    binaryOperations

--- binaryOperations.py
+++ binaryOperations.py
@@ -4,3 +4,14 @@ var_left = var << 2
 print("var_right: ",var_right)
 print("var_left: ",var_left) 
 print("var: ",var)
+x = 4
+y = 1
+ 
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+ 
+print(a, b, c, d, e, f)
